d3532cf0fdf647414f54caae4a6d7c1acaf56e63
chithranjan kumar <kumarchithranjan@gmail.com>
2022-10-21 20:51:32 +0530

    adding buttons functionality to show and hide the cart items

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,7 +1,7 @@
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
 
-const Cart = () => {
+const Cart = (props) => {
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((items) => (
@@ -11,15 +11,15 @@ const Cart = () => {
   );
 
   return (
-    <Modal>
+    <Modal onClick={props.onHideCart}>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>36.75</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button--alt"]}>Order</button>
-        <button className={classes.button}>Cancel</button>
+        <button className={classes.button}>Order</button>
+        <button className={classes["button--alt"]} onClick={props.onHideCart}>Close</button>
       </div>
     </Modal>
   );
